d0ee4f872580549cbe3461d3deb8d8804f53043c
winstxnhdw <winstxnhdw@gmail.com>
2022-01-26 01:29:31 +0800

    trying scenes

--- src/interfaces/bot.ts
+++ src/interfaces/bot.ts
@@ -1,16 +1,21 @@
+import { Telegraf, Context, Scenes, session } from 'telegraf'
 import { TOKEN, SERVER_URL, URI } from '../config/environment'
-import { Telegraf } from 'telegraf'
+import login from './scenes/login'
 import { start, help, other } from './handlers'
-
 export default class TelegramBot {
-    public tg: any
+    public tg: Telegraf<Context>
+    private stage: any
 
     constructor() {
         this.tg = new Telegraf(TOKEN)
         this.tg.telegram.setWebhook(`${SERVER_URL}${URI}`)
+
+        this.stage = new Scenes.Stage([login])
     }
 
     public async init() {
+        this.tg.use(session())
+        this.tg.use(this.stage.middleware())
         this.tg.start(start)
         this.tg.help(help)
         this.tg.on('message', other)
